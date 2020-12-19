db4a669a50592303cc4a42084f3834aeffa6c3b8
Nick Swink <nickswink7@gmail.com>
2020-12-18 22:38:43 -0500

    material ui components, basic schematic

--- src/App.js
+++ src/App.js
@@ -1,7 +1,28 @@
 import React from 'react';
+import Appbar from './components/Appbar';
+import AvatarList from './components/AvatarList';
+import Grid from '@material-ui/core/Grid';
+
 
 const App = () => {
-  return <h1>App</h1>;
+  // const [items, setItems] = useState({});
+
+  return (
+    <>
+      <Grid container>
+        <Appbar />
+        <Grid container spacing={1}>
+          <Grid container item xs={12} sm={2} spacing={1}>
+          </Grid>
+          <Grid container item xs={12} sm={8} spacing={1} justify='center'>
+            <AvatarList />
+          </Grid>
+          <Grid container item xs={12} sm={2} spacing={1}>
+          </Grid>
+        </Grid>
+      </Grid>
+    </>
+  );
 };
 
 export default App;
